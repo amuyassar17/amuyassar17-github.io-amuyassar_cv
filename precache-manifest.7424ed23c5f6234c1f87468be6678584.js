baf0b1396ee7c95ac10510726f7ff3621f53f586
amuyassar17 <amuyassar16@gmail.com>
2021-11-13 12:03:55 +0700

    Updates

---
+++ precache-manifest.7424ed23c5f6234c1f87468be6678584.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f8a1671e7252c45657b33a9af1a49d",
+    "url": "/amuyassar_cv/index.html"
+  },
+  {
+    "revision": "cecade6eedec54abfa5c",
+    "url": "/amuyassar_cv/static/css/2.cbc5e724.chunk.css"
+  },
+  {
+    "revision": "d28ba111601e82ddf56d",
+    "url": "/amuyassar_cv/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "cecade6eedec54abfa5c",
+    "url": "/amuyassar_cv/static/js/2.6e17fb0c.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/amuyassar_cv/static/js/2.6e17fb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28ba111601e82ddf56d",
+    "url": "/amuyassar_cv/static/js/main.a9a825be.chunk.js"
+  },
+  {
+    "revision": "14a1b90f82de3cb9ec1b",
+    "url": "/amuyassar_cv/static/js/runtime-main.f2969c05.js"
+  },
+  {
+    "revision": "8f65ed4eed2408b76e81fd7ec46551d2",
+    "url": "/amuyassar_cv/static/media/ahmad.8f65ed4e.jpg"
+  }
+]);
